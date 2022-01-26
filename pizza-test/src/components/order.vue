e5a044f0ac5b5e089bbb08c2548632125ee0d17b
Chris Legassick <legassickdesign@gmail.com>
2022-01-26 20:22:33 +0000

    init from Chris

---
+++ pizza-test/src/components/order.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Ordering</h1>
+    <h2>This is a new state</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Ordering"
+};
+</script>
+
+<style>
+</style>
